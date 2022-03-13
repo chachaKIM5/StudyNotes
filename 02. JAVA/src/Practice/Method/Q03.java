f130ca426c39e384bd3021ab0b0e39cc5c9bb6b2
CHACHA <101179491+chachaKIM5@users.noreply.github.com>
2022-03-13 21:51:04 +0900

    22/03/13 표현 방법 수정
    더 간결한 방식으로 업데이트

--- 02. JAVA/src/Practice/Method/Q03.java
+++ 02. JAVA/src/Practice/Method/Q03.java
@@ -8,19 +8,13 @@ public class Q03 {
 	/*
 	 
 	 프로세스
-	 1. 결과를 출력하는 메소드 선언
-	 2. 조건 연산자 이용
-	 	4자리(1000) 이상이면 그대로 출력,
-	  	4자리보다 작은 3자리 수일 때 "0" + 문자열로 바꾼 숫자 출력
-	  	3자리보다 작은 2자리 수일 때 "00" + 문자열로 바꾼 숫자 출력
-	  	2자리보다 작은 1자리 수일 때 "000" + 문자열로 바꾼 숫자 출력
-	  	나머지 음수인 경우 "음수" 메시지 출력
-	 3. snum = num을 문자열로 바꾸기
-	 4. printf로 "입력받은 숫자 -> 결과값" 출력되도록 코딩 > 호출부 입력하고 실행
+	 1. return값 없이 바로 결과를 출력하는 메소드 선언
+	 2. 형식 문자 확장 기능 이용해 4자리 숫자까지 0을 붙여 표시하기
+	 4. printf로 "입력받은 숫자 -> 결과값" 출력되도록 코딩 > 호출부 입력하고 실행 확인
 	  	
 	  
-	  
 	 */
+		
 		digit(1);
 		digit(12);
 		digit(321);
@@ -32,16 +26,20 @@ public class Q03 {
 	
 	public static void digit(int num) {
 		
-		String snum = String.valueOf(num);
-		String result = (num >= 1000 ? snum
-						: num >= 100 ? "0" + snum
-						: num >= 10 ? "00" + snum
-						: num >= 0 ? "000" + snum : "음수");
+		//수정 후
 				
-		System.out.printf("%d -> %s\n", num, result);
+		System.out.printf("%d -> %04d\n", num, num);
 		
-	}
-	
-	
-	
+	}	
 }
+
+/*
+ - 수정 전
+String snum = String.valueOf(num);
+String result = (num >= 1000 ? snum
+				: num >= 100 ? "0" + snum
+				: num >= 10 ? "00" + snum
+				: num >= 0 ? "000" + snum : "음수");
+		
+System.out.printf("%d -> %s\n", num, result);
+*/

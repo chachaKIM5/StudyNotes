e6987680c9141c71cde088297aca8b8a01f250db
CHACHA <101179491+chachaKIM5@users.noreply.github.com>
2022-03-14 23:46:37 +0900

    22/03/14 더 간결한 방식으로 수정

--- 02. JAVA/src/Practice/Overloading/Q03.java
+++ 02. JAVA/src/Practice/Overloading/Q03.java
@@ -5,13 +5,14 @@ public class Q03 {
 	public static void main(String[] args) {
 		
 	/*
-	
-	프로세스
+	 
+	 프로세스
 	 1. 결과를 반환하는 메소드 5개 선언
 	 	(메소드명 positive, 매개변수 int n1 ~ n1, n2, n3, n4, n5)
-	 2. 메소드 바디는 조건 연산자로 result = n1~n5 >= 0일 경우 +1씩... int result값 반환
-	 3. 메인 메소드에서 sum 호출, printf(양수: %d개)
-
+	 2. 메소드 바디에 int count 선언 및 0으로 초기화,
+	 	조건 연산자로 count = n1 ~ n5 > 0일 경우 증감 연산자로 +1씩... int count값 반환
+	 3. 메인 메소드에서 positive 호출, printf(양수: %d개\n)
+	 
 	 */
 
 	int count = positive(10);
@@ -19,7 +20,7 @@ public class Q03 {
 
 	count = positive(10, 20);
 	System.out.printf("양수: %d개\n", count);
-	
+
 	count = positive(10, 20, -30);
 	System.out.printf("양수: %d개\n", count);
 
@@ -28,46 +29,60 @@ public class Q03 {
 
 	count = positive(10, 20, -30, 40, 50);
 	System.out.printf("양수: %d개\n", count);
-		
+
 	} //main
-	
+
 	public static int positive(int n1) {
-		int result = (n1 > 0) ? 1 : 0 ;
-		
-		return result;
+
+	int count = 0;
+	count = (n1 > 0) ? ++count : count; 
+
+	return count;
 	}
 
 	public static int positive(int n1, int n2) {
-		int result = (n1 > 0) ? 1 : 0 ;		
-			result = (n2 > 0) ? (result + 1) : result;	
-		
-		return result;
+
+	int count = 0;
+
+	count = (n1 > 0) ? ++count : count; 
+	count = (n2 > 0) ? ++count : count; 
+
+	return count;
 	}
-	
+
 	public static int positive(int n1, int n2, int n3) {
-		int result = (n1 > 0) ? 1 : 0 ;	
-			result = (n2 > 0) ? (result + 1) : result;
-			result = (n3 > 0) ? (result + 1) : result;
-		
-		return result;
+
+	int count = 0;
+
+	count = (n1 > 0) ? ++count : count; 
+	count = (n2 > 0) ? ++count : count; 
+	count = (n3 > 0) ? ++count : count; 
+
+	return count;
 	}
-	
+
 	public static int positive(int n1, int n2, int n3, int n4) {
-		int result = (n1 > 0) ? 1 : 0 ;	
-			result = (n2 > 0) ? (result + 1) : result;
-			result = (n3 > 0) ? (result + 1) : result;
-			result = (n4 > 0) ? (result + 1) : result;
-		
-		return result;
+
+	int count = 0;
+
+	count = (n1 > 0) ? ++count : count; 
+	count = (n2 > 0) ? ++count : count; 
+	count = (n3 > 0) ? ++count : count; 
+	count = (n4 > 0) ? ++count : count; 
+
+	return count;
 	}
-	
+
 	public static int positive(int n1, int n2, int n3, int n4, int n5) {
-		int result = (n1 > 0) ? 1 : 0 ;	
-			result = (n2 > 0) ? (result + 1) : result;
-			result = (n3 > 0) ? (result + 1) : result;
-			result = (n4 > 0) ? (result + 1) : result;
-			result = (n5 > 0) ? (result + 1) : result;
-		
-		return result;
+
+	int count = 0;
+
+	count = (n1 > 0) ? ++count : count; 
+	count = (n2 > 0) ? ++count : count; 
+	count = (n3 > 0) ? ++count : count; 
+	count = (n4 > 0) ? ++count : count; 
+	count = (n5 > 0) ? ++count : count; 
+
+	return count;
 	}
 }

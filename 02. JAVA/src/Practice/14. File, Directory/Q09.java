f0f9edd5e976558e650f571f6f7baa18cbfa6386
CHACHA <101179491+chachaKIM5@users.noreply.github.com>
2022-04-05 01:59:45 +0900

    22/04/04 파일, 디렉토리 조작 연습 과제 Q9 & Q10 Update

---
+++ 02. JAVA/src/Practice/14. File, Directory/Q09.java
@@ -0,0 +1,128 @@
+package com.test.java.question.File;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Q09 {
+
+	private static ArrayList<File> totalList = new ArrayList<File>();
+
+	public static void main(String[] args) {
+		
+		
+		//폴더 내의 모든 파일들을 찾아 크기를 비교하고, 크기가 큰 순으로 정렬하시오.
+		//자식 폴더내의 파일도 모두 검색하시오.
+		//부모 자식 폴더에 상관없이 파일을 한번에 비교하시오.
+		
+		//C:\class\java\파일_디렉토리_문제\크기 정렬
+		
+		/*
+		 
+		 프로세스 설계
+		 
+		 1. static 객체 멤버 ArrayList[] totalList, PATH (경로) 설정, exists로 경로 유효성 검사
+		 
+		 2. 메소드 4개 생성
+		 	- getAllFile: ArrayList[] totaList에 모든 파일 넣기 (Q08과 같다)
+		 	- sort: totalList.get(j).length() > File 객체의 사이즈에 따라
+		 	 		totalList.get(j) > File 객체의 버블 정렬
+		  	- getLength: 파일의 크기 단위 변환해 가져오는 메소드 (Q01과 같다)
+		  	- getParnet: 파일의 경로에서 바로 위 부모 폴더 이름만 따오는 메소드
+		  	
+		 3. 형식 문자 적절히 조절해 예쁘게 출력 
+		  
+		 */
+		
+		
+		String PATH = "C:\\class\\java\\파일_디렉토리_문제\\크기 정렬";
+		File dir = new File(PATH);
+		
+		
+		if (dir.exists()) {
+			
+			
+			getAllFile(dir);
+			sort();
+			
+			
+			
+		System.out.println("[파일명]\t\t\t\t[크기]\t\t[파일이 들어있는 폴더]");
+		
+		for (File file : totalList) {
+			System.out.printf("%-30s\t%5s\t\t%s\n"
+										, file.getName()
+										, getLength(file)
+										, getParent(file));
+		}
+		
+			
+		} else {
+			System.out.println("잘못된 경로입니다.");
+		}
+		
+	}
+	
+	
+	public static void getAllFile(File dir) {
+	
+	
+		File[] list = dir.listFiles();
+			
+		for (File file : list) {
+			
+			if (file.isFile()) {
+				totalList.add(file);
+			}
+			
+			if (file.isDirectory()) {
+				getAllFile(file);
+			}
+		}
+		
+	}
+
+	
+	public static void sort() {
+		
+		for (int i=0 ; i<totalList.size()-1 ; i++) {
+			for (int j=0 ; j<totalList.size()-i-1 ; j++) {
+				
+				if (totalList.get(j).length() < totalList.get(j+1).length()) {
+					
+					File temp = totalList.get(j);
+					totalList.set(j, totalList.get(j+1));
+					totalList.set(j+1, temp);
+				}
+				
+			}
+		}
+		
+	}
+	
+	
+	public static String getLength(File file) {	
+		
+		long length = file.length();
+		String[] unit = { "B", "KB", "MB", "GB", "TB" };
+		int unitIndex = 0;
+		
+		while (length >= 1024) {
+	
+			length = length / 1024;
+			unitIndex++;
+		}
+		
+		return String.format("%d%s", length, unit[unitIndex]);
+	}
+
+	
+	
+	public static String getParent(File file) {
+		
+		String PATH = file.getParent();
+		int index = PATH.lastIndexOf("\\") + 1;
+		
+		return PATH.substring(index);
+		
+	}
+}
---
+++ 02. JAVA/src/Practice/14. File, Directory/Q10.java
@@ -0,0 +1,79 @@
+package com.test.java.question.File;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Q10 {
+
+	private static HashSet<File> set = new HashSet<File>();
+	
+	public static void main(String[] args) {
+		
+		//아래의 조건에 따라 파일을 폴더별로 분류/이동하시오.
+		//1차 : 직원 이름으로 폴더 생성
+		//2차 : 년도별로 폴더 생성
+		//3차 : 각 파일을 직원 > 년도 폴더에 이동
+		//C:\class\java\파일_디렉토리_문제\직원
+		
+		/*
+		 
+		 프로세스 설계
+		 
+		 1. main class 멤버 메소드 HashSet<File> 생성해 File이면 다 넣는다
+		 2. while 루프 > iterator로 하나씩 꺼내면서 앞 3자리 이름과 연도를 검사
+		 3. getMEMBERDIR 메소드: 각 파일이 들어가야 할 경로 (= 만들어져야 할 폴더 경로) 반환
+		 4. (3)에 따라 적절한 폴더가 없으면(!exists) 생성
+		 5. 폴더가 준비되면 (3)의 경로에서 파일명을 더해 renameTo로 이동시킨다
+		  
+		 */
+		
+		String PATH = "C:\\class\\java\\파일_디렉토리_문제\\직원";
+		File dir = new File(PATH);
+		
+		File[] list = dir.listFiles();
+		
+		//(1)
+		for (File file : list) {
+			if (file.isFile()) {
+				set.add(file);
+			}
+		}
+		
+		
+		
+		//(2)
+		
+		Iterator<File> iter = set.iterator();
+		
+		
+		while (iter.hasNext()) {
+			
+			File next = iter.next();
+			File memberDir = new File(getMEMBERDIR(next));						//(3)
+			
+			
+			if (!memberDir.exists()) {											//(4)
+				memberDir.mkdirs();
+			}
+			
+			
+			File member = new File(memberDir + "\\" + next.getName()); 			//(5)
+			next.renameTo(member);
+		}
+		
+		
+		
+	}
+	
+	public static String getMEMBERDIR(File next) {
+		
+		int index = next.getName().indexOf("_");
+		
+		String name = next.getName().substring(0, index);
+		String year = next.getName().substring(index+1, index+5);
+		
+		return "C:\\class\\java\\파일_디렉토리_문제\\직원\\" + name + "\\" + year; 
+		
+	}
+}

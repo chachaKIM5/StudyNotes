d84a6f68b9db6eaba983eeda65469212fdd7f6e5
CHACHA <101179491+chachaKIM5@users.noreply.github.com>
2022-05-03 23:59:47 +0900

    22/05/03 수업 내용 필기

---
+++ 03. Oracle/ex28_oracle_cloud.sql
@@ -0,0 +1,16 @@
+-- ex28_oracle_cloud.sql
+
+/*
+
+    오라클 클라우드 서비스
+    https://www.oracle.com/kr/cloud/free/
+    
+    오라클 클라우드에서 수업할 내용 2가지
+    - VM(Virtual Machine) 가상 컴퓨터 > 2대까지 > 나중에 웹 수업 > 프로젝트 운영 or 포트폴리오(웹)
+    - Oracle Autonomous Database 2개 > 11g > 19c
+    
+    - 무료 서비스 > 1인 사용 적당, 여러 명 사용은 버벅임
+    - 가입할 때 홈 영역은 대한민국 춘천으로 하는 편이 좋다
+
+
+*/
